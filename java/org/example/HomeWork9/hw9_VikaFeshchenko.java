5bb14fd327154b65e24614633c92bc3b2adef276
VictoriiaFeshchenko <vikafeshch@gmail.com>
2023-05-10 13:55:16 +0300

    changed 2 times HW9

--- java/org/example/HomeWork9/hw9_VikaFeshchenko.java
+++ java/org/example/HomeWork9/hw9_VikaFeshchenko.java
@@ -2,32 +2,19 @@ package org.example.HomeWork9;
 
 import java.util.Arrays;
 import java.util.Set;
-import java.util.TreeSet;
+import java.util.HashSet;
 
 public class hw9_VikaFeshchenko {
     public static void main(String[] args) {
-        String string = "A bug is the consequence/outcome of a coding fault.";
-        printUniqWord(string);
-    }
-//print uniq words
-    public static void printUniqWord(String string){
-        Set<String> masSet = new TreeSet<>(Arrays.asList(noCharacter(string)));
-        for (String l : masSet) {
-            System.out.println(capitalize(l));
-        }
-    }
-//finding characters and replacing their
-    private static String[] noCharacter(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ',' || str.charAt(i) == '.' || str.charAt(i) == '/') {
-                str = str.replace(str.charAt(i), ' ');
-            }
-        }
-        return str.toLowerCase().split(" ");
+        printUniqWord("milk, milk, beer, water");
     }
 
-    //capitalizing the first characters
-    public static String capitalize(String string) {
-        return string.substring(0, 1).toUpperCase() + string.substring(1);
+//print unique words
+    public static void printUniqWord(String string){
+        Set<String> masSet = new HashSet<>(Arrays.asList(string.replace(",", "").replace(".","").split(" ")));
+        for (String l : masSet) {
+            System.out.println(l.substring(0, 1).toUpperCase() + l.substring(1));
+        }
     }
+
 }
--- java/org/example/HomeWork9/hw9_VikaFeshchenko_In_process.java
+++ java/org/example/HomeWork9/hw9_VikaFeshchenko_In_process.java
@@ -6,10 +6,14 @@ import java.util.TreeSet;
 
 public class hw9_VikaFeshchenko_In_process {
     public static void main(String[] args) {
-        String string = "A bug is the consequence/outcome of a coding fault.";
+        String result = "AniMaL ".trim().toLowerCase().replace('a', 'A');
+        System.out.println(result);
+        String string = "milk, milk, beer, water.";
+        String string1 = "A bug is the consequence/outcome of a coding fault.";
         System.out.println(string);
-    //    String[] massive = noCharacter(string);
-        printUniqWord((string));
+     //   String[] massive = noCharacter(string);
+       printUniqWord(string);
+       printUniqWord(string1);
 
     }
 
@@ -23,16 +27,16 @@ public class hw9_VikaFeshchenko_In_process {
     }
 //    finding characters and replacing their
     private static String[] noCharacter(String str) {
-
-        for (int i = 0; i < str.length(); i++) {
+        str=str.replace(",", "").replace(".", "").replace("/", "");
+      /*  for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == ',' || str.charAt(i) == '.' || str.charAt(i) == '/') {
                // char c = str.charAt(i);
                 System.out.println(str.charAt(i));
-                str = str.replace(str.charAt(i), ' ');
+                str = str.replace(",","").replace(".","").replace("/","");
                 System.out.println("sp" + str.charAt(i) + "sp");
             }
-        }
-        System.out.println(str);
+        }*/
+        System.out.println("9999"+str);
         return str.toLowerCase().split(" ");
     }
 
